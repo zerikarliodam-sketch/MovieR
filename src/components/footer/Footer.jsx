2c9c7a08a3226d2b16eaf3d0db192ff077e2c18e
Spicy - musicenter <zerikarliodam@gmail.com>
2025-12-20 21:56:29 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,30 +14,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
                 <div className="infoText">
-                    
+                  ОНЛАЙН МЕДИАТЕКА 
                 </div>
-                <div className="socialIcons">
-                    <span className="icon fb ">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon insta">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon twitt">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon link">
-                        <FaLinkedin />
-                    </span>
-                </div>
+                
+               
             </ContentWrapper>
         </footer>
     );
