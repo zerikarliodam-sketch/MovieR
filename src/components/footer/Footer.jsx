1e769a237e89e92c2873c037f62b78f8ff59fa70
Spicy - musicenter <zerikarliodam@gmail.com>
2025-12-20 21:43:35 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,35 +12,6 @@ import "./Footer.scss";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    
-Netlivy TV — онлайн медиатека и телепередачи для комфортного просмотра в любое время 🌛 и на любом устройстве 📱💻. Смотрите любимые телеканалы, фильмы и передачи в высоком качестве 🔥🎬 с быстрым и стабильным онлайн-доступом ⚡ Почувствуйте бесконечность вместе с нами❤️‍🔥
-                </div>
-                <div className="socialIcons">
-                    <span className="icon fb ">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon insta">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon twitt">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon link">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
     );
 };
 
